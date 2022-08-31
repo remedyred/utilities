b939514b71d9d8857fdcedde782f34cfd9306fa9
Snickbit <snickbit@users.noreply.github.com>
2022-08-30 22:56:25 -0400

    fix: dts generation (#56)

--- __tests__/numbers.test.ts
+++ __tests__/numbers.test.ts
@@ -32,6 +32,7 @@ describe('numbers', () => {
 
 	describe('numberEven', () => {
 		it.each([
+			[0, 0],
 			[1, 2],
 			[2, 2],
 			[3, 4],
@@ -39,16 +40,37 @@ describe('numbers', () => {
 		])('should return %d as the even number %d', (subject, result) => {
 			expect(numberEven(subject)).toBe(result)
 		})
+
+		it.each([
+			[0, 0],
+			[1, 0],
+			[2, 2],
+			[3, 2],
+			[4, 4]
+		])('should return %d as the even number %d by subtraction', (subject, result) => {
+			expect(numberEven(subject, true)).toBe(result)
+		})
 	})
 
 	describe('numberOdd', () => {
 		it.each([
+			[0, 1],
 			[1, 1],
 			[2, 3],
 			[3, 3],
 			[4, 5]
 		])('should return %d as the odd number %d', (subject, result) => {
 			expect(numberOdd(subject)).toBe(result)
+		})
+
+		it.each([
+			[0, -1],
+			[1, 1],
+			[2, 1],
+			[3, 3],
+			[4, 3]
+		])('should return %d as the odd number %d', (subject, result) => {
+			expect(numberOdd(subject, true)).toBe(result)
 		})
 	})
 })
